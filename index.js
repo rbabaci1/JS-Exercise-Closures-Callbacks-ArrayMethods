a581ec2578ae42b080637774ea0edfc834a180c6
rabah babaci <4rabah@gmail.com>
2019-12-05 18:58:10 -0800

    update counterMakerWithLimit

--- index.js
+++ index.js
@@ -291,8 +291,14 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(max_value) {
+  let count = 0;
+  return function counter() {
+    if (count > max_value) {
+      count = 0;
+    }
+    return count++;
+  }
 }
 
 /////////////// END OF CHALLENGE ///////////////
